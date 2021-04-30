4b47cb194ab3e6f46d992b39f4587513396c3640
Keith Mascarenhas <kmascare@akamai.com>
2021-04-29 21:56:56 -0400

    fix moveparser to read moves with leading/trailing whitespaces

--- move_parser.py
+++ move_parser.py
@@ -6,6 +6,7 @@ PATTERN = re.compile('^([Oo0](-[Oo0]){1,2}|[KkQqRrBbNn]?[a-h]?[1-8]?x?[a-h][1-8]
 def detect_move(move):
   # check for "Go Nb5"
     move = move.lower()
+    move = move.strip()
     move = move.replace('x','')
     move = move.replace('×','')
     move = move.replace('#','')
